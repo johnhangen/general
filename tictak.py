267ba614bdcfe2093e70a7e3837c8820264965bf
johnhangen <68433301+johnhangen@users.noreply.github.com>
2021-05-15 10:27:28 -0400

    Update tictak.py

--- tictak.py
+++ tictak.py
@@ -54,40 +54,86 @@ def player_move():
 
 # somthing is still wrong
 def computer_move():
-  while(game_over() == False):
     pick = random.randrange(1, 9, 1)
-    print(pick)
     if (pick == 1 or pick == 2 or pick == 3):
       if(board[0][pick - 1] == '-' and places[0][pick - 1] != None):
         places[0][pick - 1] = None
         board[0][pick - 1] = 'X'
-        break
       else:
         computer_move()
     elif (pick == 4 or pick == 5 or pick == 6):
       if(board[1][pick - 4] == '-' and places[1][pick - 4] != None):
         places[1][pick - 4] = None
         board[1][pick - 4] = 'X'
-        break
       else:
         computer_move()
     elif (pick == 7 or pick == 8 or pick == 9):
       if(board[2][pick - 7] == '-' and places[2][pick - 7] != None):
         places[2][pick - 7] = None
         board[2][pick - 7] = 'X'
-        break
       else:
         computer_move()
-  else:
-    print(f"computer chose {pick}")
 
 
 
 def game_over():
-  return False
+    if (board[0][0] == 'X' and board[0][1] == 'X' and board[0][2] == 'X'):
+        print("computer won")
+        return True
+    elif (board[0][0] == 'O' and board[0][1] == 'O' and board[0][2] == 'O'):
+        print("you won!")
+        return True
+    elif (board[1][0] == 'X' and board[1][1] == 'X' and board[1][2] == 'X'):
+        print("computer won")
+        return True
+    elif (board[1][0] == 'O' and board[1][1] == 'O' and board[1][2] == 'O'):
+        print("you won!")
+        return True
+    elif (board[2][0] == 'X' and board[2][1] == 'X' and board[2][2] == 'X'):
+        print("computer won")
+        return True
+    elif (board[2][0] == 'O' and board[2][1] == 'O' and board[2][2] == 'O'):
+        print("you won!")
+        return True
+    elif (board[0][0] == 'X' and board[1][0] == 'X' and board[2][0] == 'X'):
+        print("computer won")
+        return True
+    elif (board[0][0] == 'O' and board[1][0] == 'O' and board[2][0] == 'O'):
+        print("you won!")
+        return True
+    elif (board[0][1] == 'X' and board[1][1] == 'X' and board[2][1] == 'X'):
+        print("computer won")
+        return True
+    elif (board[0][1] == 'O' and board[1][1] == 'O' and board[2][1] == 'O'):
+        print("you won!")
+        return True
+    elif (board[0][2] == 'X' and board[1][2] == 'X' and board[2][2] == 'X'):
+        print("computer won")
+        return True
+    elif (board[0][2] == 'O' and board[1][2] == 'O' and board[2][2] == 'O'):
+        print("you won!")
+        return True
+    elif (board[0][0] == 'X' and board[1][1] == 'X' and board[2][2] == 'X'):
+        print("computer won")
+        return True
+    elif (board[0][0] == 'O' and board[1][1] == 'O' and board[2][2] == 'O'):
+        print("you won!")
+        return True
+    elif (places[0][0] == None and places[0][1] == None and places[0][2] == None and places[1][0] == None and places[1][1] == None and places[1][2] == None and places[2][0] == None and places[2][1] == None and places[2][2] == None):
+        print("tie")
+        return True
+    else:
+        return False
 
-while(game_over() == False):
+game_state = game_over()
+while(game_state == False):
   player_move()
+  if (game_state == True):
+      break
   computer_move()
+  print("")
   print_board(board)
-  print_pos(places)
+  print("----------------------")
+  game_state = game_over()
+
+print("game over")
