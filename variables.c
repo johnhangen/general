6ac04aefef4a2d27bafd8e1fa79f6a922942cca6
johnhangen <68433301+johnhangen@users.noreply.github.com>
2021-05-17 19:55:48 -0400

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+/* main is run when the program is run */
+int main(void)
+{
+    int x = 10; 
+    int y = x / 2;
+    printf("The value of x/2 is: %i\nWhere x is equal to: %i\n,y,x");
+    return 0;
+}
