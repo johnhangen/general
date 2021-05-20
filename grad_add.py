e9d3c3d302a97946888d3b4a20b5a7a172fc55ed
johnhangen <68433301+johnhangen@users.noreply.github.com>
2021-05-19 20:34:07 -0400

    Update grad_add.py

--- grad_add.py
+++ grad_add.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
 import numpy as np
 import pandas as pd
 
@@ -7,7 +9,13 @@ grad_data = pd.read_csv("other/Admission_Predict.csv")
 gre_score = grad_data["GRE Score"]
 addmision_chance = grad_data["Chance of Admit "]
 
-plt.plot(gre_score, addmision_chance * 100, 'ro')
+x = np.array(addmision_chance).reshape((-1,1))
+y = np.array(gre_score)
+
+model = LinearRegression().fit(x,y)
+y_pred = model.predict(x)
+
+plt.plot(gre_score, addmision_chance * 100, 'ro',  y_pred, addmision_chance *100)
 plt.title("Gre Score vs Chance of Admit")
 plt.xlabel("GRE Score")
 plt.ylabel("Chance of Admission (%)")
