08d1acf664f5063f50dacdd085ba7f21147dc630
johnhangen <68433301+johnhangen@users.noreply.github.com>
2021-05-22 14:15:16 -0400

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,26 @@
+ # include <stdio.h>
+
+void swap(int *x,int  *y);
+
+int main(void)
+{
+	/* variables */
+	int x = 0;
+	int y = 1;
+
+	printf("Start: x = %i and y = %i\n",x, y);
+
+	swap(&x, &y);
+
+	printf("End: x = %i and y = %i\n",x, y);
+
+	return 0;
+}
+
+void swap(int *x,int *y)
+{
+	int temp; 
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
