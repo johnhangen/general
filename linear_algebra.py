ed170854aed969569548ca59f90ad31115fe4c50
johnhangen <68433301+johnhangen@users.noreply.github.com>
2021-05-25 20:57:49 -0400

    Update linear_algebra.py

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,8 +1,8 @@
 # import numpy
 import numpy as np
 
-vecX = np.array = [1, 2]
-vecY = np.array = [1, 2]
+vecX = np.array = [1, 1]
+vecY = np.array = [1, 1]
 
 # Calculates the dot product
 dot_product = np.dot(vecX, vecY)
@@ -10,5 +10,9 @@ dot_product = np.dot(vecX, vecY)
 # what does the dot product of the two vectors tell us
 if(dot_product == 0):
     print("The angle between the two vectors is 90 degrees")
+elif(dot_product == 1):
+    print("The angle between the two vectors is 0 degrees")
+elif(dot_product == -1):
+    print("The angle between the two vectors is 180 degrees")
 else:
     print("The angle between the two vectors is somthing other than 90 degrees")
