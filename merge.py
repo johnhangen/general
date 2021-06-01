60efe4bfbc036d82708ee693a9c8d2c2900d2a00
johnhangen <>
2021-06-01 14:10:46 +0000

    updated so it is O(n log n)

--- merge.py
+++ merge.py
@@ -1,16 +1,17 @@
 # Merge sort
 #
-# 5/12/2021
+# 6/1/2021
 # @author Jack Hangen
-# Incomplete
+# Complete
 # Comparing big O of Merge sort to actual
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import math
 
 def mergeSort(arr):
     comp = 0
+    comp += 1
     if len(arr) > 1:
  
         mid = len(arr)//2
@@ -26,6 +27,7 @@ def mergeSort(arr):
         i = j = k = 0
  
         while i < len(L) and j < len(R):
+            comp += 1
             if L[i] < R[j]:
                 arr[k] = L[i]
                 i += 1
@@ -50,8 +52,9 @@ def mergeSort(arr):
 
 
 size = [10, 30, 50, 70, 100, 300, 500, 700, 1000, 3000, 5000, 7000, 10000]
-comps = []
-temp_array = [10 * 10, 30 * 30, 50 *50, 70 * 70, 100 * 100, 300 * 300, 500 *500, 700 * 700, 1000 * 1000, 3000 * 3000, 5000 *5000, 7000 * 7000, 10000 * 10000]
+comps = [] 
+temp_array = [10 * math.log(10), 30 * math.log(30), 50 * math.log(50), 70 * math.log(70), 100 * math.log(100), 300 * math.log(300), 500 * math.log(500), 700 * math.log(700), 1000 * math.log(1000), 3000 * math.log(30000), 5000 * math.log(50000), 7000 * math.log(70000), 10000 * math.log(100000)]
+print(math.log(300), math.log(500), math.log(700), math.log(1000))
 
 # runs the selection sort method for everything in the array size and stores it in comps
 for i in size:
@@ -64,7 +67,6 @@ for i in size:
 plt.plot(size, comps, 'r--', size, temp_array, 'b--')
 plt.ylabel("Number of comparisons")
 plt.xlabel("Size of array")
-plt.title('Insertion Sort Comparisons vs Big O')
-plt.text(1000, 90000000, "red = Insertion, blue = big O")
-plt.axis([0, 10000,0,100000000])
-plt.show()   
+plt.title('mergeSort Sort Comparisons vs Big O')
+plt.text(1000, 90000000, "red = mergeSort, blue = big O")
+plt.show()
