531e9fc0855e468eac0b43bcb9112b70f5284406
johnhangen <68433301+johnhangen@users.noreply.github.com>
2021-06-10 23:52:41 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -8,6 +8,7 @@
 # import statments
 import tkinter
 import random
+import time
 
 # var
 board = [['-','-','-'],['-','-','-'],['-','-','-']]
@@ -15,7 +16,10 @@ places = [[1,2,3],[4,5,6],[7,8,9]]
 places_set = [[1,2,3],[4,5,6],[7,8,9]]
 places2 = [1,2,3,4,5,6,7,8,9]
 
-# creation of tiktaktoe funtions
+# create instance
+window = tkinter.Tk()
+window.title("Tik Tak Toe")
+
 def player_move(pos):
   if (pos == 1 or pos == 2 or pos == 3):
     if(board[0][pos - 1] == '-' and places[0][pos - 1] != None):
@@ -23,7 +27,7 @@ def player_move(pos):
       board[0][pos - 1] = 'O'
       return True
     else:
-      print(f"{pos} is not an avaliable possition")
+      print(f"{pos} is not an avaliable possition: ERROR 1")
       return False
   elif (pos == 4 or pos == 5 or pos == 6):
     if(board[1][pos - 4] == '-' and places[1][pos - 4] != None):
@@ -31,7 +35,7 @@ def player_move(pos):
       board[1][pos - 4] = 'O'
       return True
     else:
-      print(f"{pos} is not an avaliable possition")
+      print(f"{pos} is not an avaliable possition: ERROR 1")
       return False
   elif (pos == 7 or pos == 8 or pos == 9):
     if(board[2][pos - 7] == '-' and places[2][pos - 7] != None):
@@ -39,10 +43,10 @@ def player_move(pos):
       board[2][pos - 7] = 'O'
       return True
     else:
-      print(f"{pos} is not an avaliable possition")
+      print(f"{pos} is not an avaliable possition: ERROR 1")
       return False
   else:
-    print(f"{pos} is not an avaliable possition")
+    print(f"{pos} is not an avaliable possition: ERROR 2")
     return False
 
 # computer turn
@@ -121,10 +125,60 @@ def game_over():
     else:
         return False
 
-# create instance
-window = tkinter.Tk()
+def convert(num):
+    if(num == "1"):
+        return 1
+    elif(num == "2"):
+        return 2
+    elif(num == "3"):
+        return 3
+    elif(num == "4"):
+        return 4
+    elif(num == "5"):
+        return 5
+    elif(num == "6"):
+        return 6
+    elif(num == "7"):
+        return 7
+    elif(num == "8"):
+        return 8
+    elif(num == "9"):
+        return 9
+    else:
+        print("I have no fucking clue now")
 
-# create wiggets
+count1 = 0
+count = 0
+
+# click
+def click():
+    output = num.get()
+    try:
+        global count
+        global count1
+        if (game_over == True):
+            time.sleep(5)
+            window.destroy()
+            exit()
+        output = convert(output)
+        while (player_move(output) == True):
+            print(xs[count])
+            placeWigget(output, xs[count])
+        count = count + 1
+        if (game_over == True):
+            time.sleep(5)
+            window.destroy()
+            exit()
+        else:
+            compMove = computer_move()
+            print(compMove)
+            placeWigget(compMove, os[count1])
+            count1 = count1 + 1
+    except:
+        print("I dont fucking know 168")
+        time.sleep(5)
+        window.destroy()
+        exit()
 
 # makes the four lines
 line_one = tkinter.Label(
@@ -161,123 +215,63 @@ title = tkinter.Label(
 
 # for x and o
 x = tkinter.Label(
-    text="x"
+    text="x", 
+    font="none 60 bold"
+)
+
+x1 = tkinter.Label(
+    text="x", 
+    font="none 60 bold"
+)
+
+x2 = tkinter.Label(
+    text="x", 
+    font="none 60 bold"
+)
+
+x3 = tkinter.Label(
+    text="x", 
+    font="none 60 bold"
+)
+
+x4 = tkinter.Label(
+    text="x", 
+    font="none 60 bold"
 )
 
 o = tkinter.Label(
-    text="o"
+    text="o",
+    font="none 60 bold"
+)
+o1 = tkinter.Label(
+    text="o",
+    font="none 60 bold"
+)
+o2 = tkinter.Label(
+    text="o",
+    font="none 60 bold"
+)
+o3 = tkinter.Label(
+    text="o",
+    font="none 60 bold"
+)
+o4 = tkinter.Label(
+    text="o",
+    font="none 60 bold"
 )
 
-num = tkinter.Listbox()
-num.insert(1, "1")
-num.insert(2, "2")
-num.insert(3, "3")
-num.insert(4, "4")
-num.insert(5, "5")
-num.insert(6, "6")
-num.insert(7, "7")
-num.insert(8, "8")
-num.insert(9, "9")
+xs = [x, x1, x2, x3, x4]
+os = [o, o1, o2, o3, o4]
 
-# place x funtion
-def placeX(nums):
-    if(nums == 1):
-        x.place(
-            x = 800,
-            y = 100
-        )
-    elif(nums == 2):
-        x.place(
-            x = 1000,
-            y = 100
-        )
-    elif(nums == 3):
-        x.place(
-            x = 1200,
-            y = 100
-        )
-    elif(nums == 4):
-        x.place(
-            x = 800,
-            y = 275
-        )
-    elif(nums == 5):
-        x.place(
-            x = 1000,
-            y = 275
-        )
-    elif(nums == 6):
-        x.place(
-            x = 1200,
-            y = 275
-        )
-    elif(nums == 7):
-        x.place(
-            x = 800,
-            y = 450
-        )
-    elif(nums == 8):
-        x.place(
-            x = 1000,
-            y = 450
-        )
-    elif(nums == 9):
-        x.place(
-            x = 1200,
-            y = 450
-        )
-    else:
-        print("fucking error")
+# submit button
+button = tkinter.Button(
+    text="SUBMIT",
+    width=6,
+    command=click
+)
 
-# place 0 funtion
-def placeO(nums):
-    if(nums == 1):
-        o.place(
-            x = 800,
-            y = 100
-        )
-    elif(nums == 2):
-        o.place(
-            x = 1000,
-            y = 100
-        )
-    elif(nums == 3):
-        o.place(
-            x = 1200,
-            y = 100
-        )
-    elif(nums == 4):
-        o.place(
-            x = 800,
-            y = 275
-        )
-    elif(nums == 5):
-        o.place(
-            x = 1000,
-            y = 275
-        )
-    elif(nums == 6):
-        o.place(
-            x = 1200,
-            y = 275
-        )
-    elif(nums == 7):
-        o.place(
-            x = 800,
-            y = 450
-        )
-    elif(nums == 8):
-        o.place(
-            x = 1000,
-            y = 450
-        )
-    elif(nums == 9):
-        o.place(
-            x = 1200,
-            y = 450
-        )
-    else:
-        print("fucking error")
+# lists the numbers
+num = tkinter.Entry()
 
 # place afformentioned wiggets
 line_one.pack()
@@ -316,24 +310,62 @@ title.place(
     y = 500
 )
 
+button.pack()
+button.place(
+    x = 850,
+    y = 550
+)
 
-game_state = game_over()
-keep = False
-# main loops
-while(game_state == False):
-    while(keep == False):
-        print("got here")
-        nums = num.get(first= 0)
-        print(nums)
-        if(nums in places2):
-            keep = True
-    keep = False
-    nums = player_move(nums)
-    placeX(nums)
-    if (game_state == True):
-        break
-    nums = computer_move()
-    placeO(nums)
-    game_state = game_over()
+# creation of tiktaktoe funtions
+
+# place X and O funtion
+def placeWigget(nums, wigget):
+    if(nums == 1):
+        wigget.place(
+            x = 800,
+            y = 100
+        )
+    elif(nums == 2):
+        wigget.place(
+            x = 1000,
+            y = 100
+        )
+    elif(nums == 3):
+        wigget.place(
+            x = 1200,
+            y = 100
+        )
+    elif(nums == 4):
+        wigget.place(
+            x = 800,
+            y = 275
+        )
+    elif(nums == 5):
+        wigget.place(
+            x = 1000,
+            y = 275
+        )
+    elif(nums == 6):
+        wigget.place(
+            x = 1200,
+            y = 275
+        )
+    elif(nums == 7):
+        wigget.place(
+            x = 800,
+            y = 450
+        )
+    elif(nums == 8):
+        wigget.place(
+            x = 1000,
+            y = 450
+        )
+    elif(nums == 9):
+        wigget.place(
+            x = 1200,
+            y = 450
+        )
+    else:
+        print("fucking error 348")
 
 window.mainloop()
